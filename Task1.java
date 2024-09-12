3fb58d375677995914900d07cf7eb8e016ab4c38
Ahsan Ali <ahsan.ali429@gmail.com>
2024-09-13 00:31:29 +0500

    Task 1
    Initial commit for Task 1

---
+++ Task1.java
@@ -0,0 +1,9 @@
+public class Task1 {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            for (int k = 0; k < args.length; k++) {
+                System.out.println("Value of input is: " + k + " and arguments: " + args[k]);
+            }
+        }
+    }
+}
