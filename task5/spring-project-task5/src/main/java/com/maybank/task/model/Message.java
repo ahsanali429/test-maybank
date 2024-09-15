11b7ee51bfd83a39312996b17b6ed7e09b493389
Ahsan Ali <ahsan.ali429@gmail.com>
2024-09-16 01:09:05 +0500

    MySQL Connectivity
    Table created if not present automatically

--- task5/spring-project-task5/src/main/java/com/maybank/task/model/Message.java
+++ task5/spring-project-task5/src/main/java/com/maybank/task/model/Message.java
@@ -1,14 +1,16 @@
 package com.maybank.task.model;
 
-import org.springframework.stereotype.Component;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
 @Entity
 public class Message {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String message;
 
